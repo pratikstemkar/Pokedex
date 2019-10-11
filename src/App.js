e23bd9aa9942b420044658a66d4e28d16b6ce609
Pratik Temkar <pratiktemkarofficial@gmail.com>
2019-10-11 09:57:33 +0530

    Packages added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React, { Fragment } from 'react'
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
 
+import store from './store/store'
+import { Provider } from 'react-redux'
+
 const App = () => {
   return (
     <Fragment>
-      <Navbar />
-      <div className="container">
-        <Landing />
-      </div>
+      <Provider store={store}>
+        <Navbar />
+        <div className="container">
+          <Landing />
+        </div>
+      </Provider>
     </Fragment>
   )
 }
